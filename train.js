67f8085fffda6e1adc5f9e3d5171b23ede458ab0
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2015-02-20 16:57:07 +0000

    Update train.js

--- train.js
+++ train.js
@@ -21,7 +21,7 @@ k=function(){
 	}
 	u=(u+s/3)%w;
 	i=5;while(i--)e.push([m,z,0.8*f.f,f.f*6,f.f,20+f.f*30]);
-	c.arc(w*3-co*s/3,z/2,50,2,2*pi);
+	c.arc(w*2-co*s/3,z/2,50,2,2*pi);
 	for(i in e){
 		_=e[i];		
 		if(_[2]<0){e.splice(i ,1);i--}
